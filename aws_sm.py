5eec8cbbc36e03d567acf08c8fc5042c600d3eb1
Anssi Y-L <anssi.ylileppala@brightstraining.com>
2022-11-16 13:39:32 +0200

    Get value function created

--- aws_sm.py
+++ aws_sm.py
@@ -1,6 +1,7 @@
 import boto3
 import logging
 from botocore.exceptions import ClientError
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -17,12 +18,6 @@ def create(username, password, host):
     name = "w6pg1_rds-test11"
 
     try:
-        # kwargs = {"username":username,"password":password,"engine":"postgres","host":host,"port":"5432","dbname":"posts","dbInstanceIdentifier":"w6pg1_rds"}
-        # if isinstance(secret_value, str):
-        #     kwargs["SecretString"] = secret_value
-        # elif isinstance(secret_value, bytes):
-        #     kwargs["SecretBinary"] = secret_value
-        # response = client.create_secret(**kwargs)
         response = client.create_secret(Name = name,
                                         SecretString = '{"username":"%s","password":"%s","engine":"postgres","host":"%s","port":"5432","dbname":"posts","dbInstanceIdentifier":"w6pg1_rds"}' % (username, password, host))
         
@@ -33,6 +28,33 @@ def create(username, password, host):
     else:
         return response
 
+def get_value(name):
+        """
+        Gets the value of a secret.
+
+        :param stage: The stage of the secret to retrieve. If this is None, the
+                      current stage is retrieved.
+        :return: `SecretString` field as dictionary
+        """
+        client = boto3.client("secretsmanager")
+
+        try:
+            kwargs = {'SecretId': name}
+            response = client.get_secret_value(**kwargs)
+            logger.info("Got value for secret %s.", name)
+        except ClientError:
+            logger.exception("Couldn't get value for secret %s.", name)
+            raise
+        finally:
+            #return response
+            return json.loads(response['SecretString'])
+
 if __name__ == "__main__":
     #print(create("simo", "Salasana2", "arn:2321"))
+    #data = get_value("w6pg1_rds-test11")
+    # data = get_value("week5/group2/rds")
+    # print(data)
+    # data2 = json.loads(data['SecretString'])
+    # print(data2['username'])
+    #print(type(get_value("w6pg1_rds-test11")))
     pass
