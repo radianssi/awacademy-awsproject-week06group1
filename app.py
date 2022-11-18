f3c1c548b81ea065984ed517ab5e036083a3b75b
Santeri <santeri.nenonen@brightstraining.com>
2022-11-18 14:09:11 +0200

    Added yaml files and removed unnecessary

--- app.py
+++ app.py
@@ -122,3 +122,8 @@ def delete(id):
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
 
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
